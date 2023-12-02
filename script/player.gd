6fc3ec4dae9a365f1a61159b5a4dbd91b8caed50
Eduardo Silva <eduardofox989@protonmail.com>
2023-12-02 14:37:18 -0300

    add walljump

--- script/player.gd
+++ script/player.gd
@@ -1,24 +1,27 @@
 extends CharacterBody2D
 
-enum STATES {AIR = 1, FLOOR = 2, LADDER, WALL } # cada valor constante em um enum é associado a um situação do jogador
+enum STATES {AIR = 1, FLOOR, LADDER, WALL} # cada valor constante em um enum é associado a um situação do jogador
 const SPEED = 220.0
 const RUNSPEED = 400.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -400.0
 
 
 var playerState = STATES.AIR # inicia o jogo com o situação do player AIR ou estar no ar/voando/pulando
 var playerOnButton = false # troca para TRUE se o jogador se aproximar do botão
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var direction = 1
 @onready var animation := $sprite as AnimatedSprite2D # variavel para manipular a sprite do player
+@onready var wallchacker := $wallchecker as RayCast2D
 
 func _physics_process(delta):
+	print(is_near_wall())
 	match playerState: # swith que usa o situação atual do player
 		STATES.AIR: # caso ele esteja no ar
-			print(velocity.y)
-			print(playerState)
+			animation.play("air") # muda a sprite para pulando
 			if is_on_floor(): # se ele encostar no chão
 				playerState = STATES.FLOOR # muda a situação para FLOOR
-			animation.play("air") # muda a sprite para pulando
+			elif is_near_wall():
+				playerState = STATES.WALL
 			if Input.is_action_pressed("ui_right"): # se precionar seta direita 
 				if Input.is_action_pressed("ui_up"): # se estiver segurando o botão de corre
 					velocity.x = lerp(velocity.x,RUNSPEED,0.1) # vai para direita com a velocidade de corrida
@@ -31,12 +34,10 @@ func _physics_process(delta):
 				else:
 					velocity.x = lerp(velocity.x,-SPEED,0.1)
 				animation.flip_h = true # inverte a sprite
-			velocity.y += gravity * delta # adiciona gravidade ao player
-			move_and_slide() # atualiza o player
+			move_and_fall(delta, false) # atualiza o player
+			set_direction()
 
 		STATES.FLOOR: # caso ele esteja no chão
-			print(velocity.x)
-			print(playerState)
 			if Input.is_action_pressed("ui_right"): # se precionar seta direita
 				if Input.is_action_pressed("ui_up"): # se estiver segurando o botão de corre
 					velocity.x = lerp(velocity.x,RUNSPEED,0.1) # vai para direita com a velocidade de corrida
@@ -58,9 +59,35 @@ func _physics_process(delta):
 			if Input.is_action_pressed("ui_accept"): # se o player aperta espaço
 				velocity.y = JUMP_VELOCITY # pula
 				playerState = STATES.AIR # e muda a situação para AIR, pulando
-			move_and_slide() # atualiza o player
-			
-	print(playerOnButton)
+			if not is_on_floor():
+				playerState = STATES.AIR
+			move_and_fall(delta, false) # atualiza o player
+			set_direction()
+		STATES.WALL:
+			if is_on_floor():
+				playerState = STATES.FLOOR
+			elif not is_near_wall():
+				playerState = STATES.AIR
+			if Input.is_action_pressed("ui_accept") and ((Input.is_action_pressed("ui_left") and direction == 1) or (Input.is_action_pressed("ui_right") and direction == -1)):
+				velocity.x = 450 * -direction
+				velocity.y = JUMP_VELOCITY * 0.7
+			move_and_fall(delta, true) # atualiza o player
+	
+
+
+func move_and_fall(delta, slow_fall : bool):
+	if slow_fall:
+		velocity.y = clamp(velocity.y,JUMP_VELOCITY, 200)
+		
+	velocity.y += gravity * delta
+	move_and_slide()
+	
+func set_direction():
+	direction = 1 if not animation.flip_h else -1
+	wallchacker.rotation_degrees = 90 * -direction
+
+func is_near_wall():
+	return wallchacker.is_colliding()
 
 func _on_area_2d_area_entered(area): # se o player se aproximar do botão
 	playerOnButton = true # a variavel se torna verdadeira
